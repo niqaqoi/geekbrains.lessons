ebe3d46d1cd6a3a8fcc3ac114be393309046d6ee
niqaqoi <n.makarov89@yandex.ru>
2021-03-15 23:20:26 +0300

    full homework done

--- src/geekbrains/lessons/taskLesson5/Employee.java
+++ src/geekbrains/lessons/taskLesson5/Employee.java
@@ -1,32 +1,35 @@
 package geekbrains.lessons.taskLesson5;
 
-public class Employee {
-
-    private String fullName;
+public class Employee<staff> {
+    private String name;
     private String position;
     private String email;
     private String phoneNumber;
-    private String salary;
+    private int salary;
     private int age;
 
-    Employee(String fullName, String position, String email, String phoneNumber, String salary, int age) {
-        this.fullName = fullName;
+    Employee(String name,String position, String email, String phoneNumber, int salary, int age) {
+        this.name = name;
         this.position = position;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.salary = salary;
         this.age = age;
+    }
 
-        Employee[] employeeList = new Employee[5];
-        employeeList[0] = new Employee("Ivan Ivanov", "CEO", "ii@it.com", "987654",
-                "10000$", 45);
-        employeeList[1] = new Employee("Petr Petrov", "Senior Developer", "pp@it.com", "123456",
-                "5000$", 41);
-        employeeList[2] = new Employee("Alex Alexov", "Senior Developer", "aa@it.com", "123987",
-                "5000$", 38);
-        employeeList[3] = new Employee("Mariya Mashina", "Middle Developer", "mm@it.com", "789456",
-                "3000$", 35);
-        employeeList[4] = new Employee("Nikita Nikitin", "Junior Developer", "nn@it.com", "1987656",
-                "2000$", 31);
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
     }
 }
---
+++ src/geekbrains/lessons/taskLesson5/Main.java
@@ -0,0 +1,20 @@
+package geekbrains.lessons.taskLesson5;
+
+public class Main {
+
+    public static final int AGE = 40;
+
+    public static void main(String[] args) {
+        Employee staff[] = new Employee[5];
+        staff[0] = new Employee("Ivan", "CEO", "ivan@it.com", "9191", 10000, 41);
+        staff[1] = new Employee("Petr", "Team Lead", "petr@it.com", "9192", 5000, 38);
+        staff[2] = new Employee("Fedor", "Middle Dev", "fedor@it.com", "9193", 4000, 42);
+        staff[3] = new Employee("Alex", "Middle Dev", "alex@it.com", "9194", 4000, 35);
+        staff[4] = new Employee("Maks", "Junior Dev", "maks@it.com", "9195", 2000, 20);
+        for (int i = 0; i < staff.length; i++) {
+            if (staff[i].getAge() > AGE) {
+                System.out.println(staff[i]);;
+            }
+        }
+    }
+}
